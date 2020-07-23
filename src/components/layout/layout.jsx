c27b845428e7b72581c8e0ba8285fcc6bd068679
Sean Harte <scsharte@gmail.com>
2020-07-23 18:04:01 -0400

    add small layout styling

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,11 +1,12 @@
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import styles from './layout.module.scss';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={styles.body}>{children}</main>
       <Footer />
     </>
   );
