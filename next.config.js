6084bee4aae50c07e784eccb93d131b04606edef
Sean Harte <scsharte@gmail.com>
2020-07-25 08:14:55 -0400

    added sitemap and robots txt file

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   sassOptions: {
     postcssLoaderOptions: {},
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  },
 };
