721bb6563e50dce690c1bb059517691cb9a2fb04
Sean Harte <scsharte@gmail.com>
2020-07-26 17:52:12 -0400

    added heder mobile menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,50 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
+import DrawerMenu from '../drawerMenu/drawerMenu';
+import { useRef } from 'react';
+import { useDisclosure } from '@chakra-ui/core';
+import { IoIosMenu } from 'react-icons/io';
 
-const Header = ({ navLinks = [] }) => (
-  <header className={styles['main-header']} role="banner">
-    <div className={styles.logo}>
-      <img src="https://www.pandaipsum.com/img/panda.png" alt="logo" />
-    </div>
-    <nav className={styles['nav-container']} role="navigation">
-      <ol className={styles.list}>
-        {navLinks.map((v) => (
-          <li key={v}>
-            <Link href={v === 'index' ? '/' : `/${v}`}>
-              <a>{v === 'index' ? 'home' : v}</a>
-            </Link>
-          </li>
-        ))}
-      </ol>
-    </nav>
-  </header>
-);
+const HeaderListItems = ({ navLinks = [] }) =>
+  navLinks.map((v) => (
+    <li key={v}>
+      <Link href={v === 'index' ? '/' : `/${v}`}>
+        <a>{v === 'index' ? 'home' : v}</a>
+      </Link>
+    </li>
+  ));
+
+const Header = ({ navLinks = [] }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <>
+      <header className={styles['main-header']} role="banner">
+        <div className={styles.logo}>
+          <img src="https://www.pandaipsum.com/img/panda.png" alt="logo" />
+        </div>
+        <nav className={styles['nav-container']} role="navigation">
+          <button
+            className={styles['toggle-menu']}
+            ref={btnRef}
+            onClick={onOpen}
+          >
+            <IoIosMenu />
+            <div className={styles['sub-text']}>menu</div>
+          </button>
+          <ol className={styles.list}>
+            <HeaderListItems navLinks={navLinks} />
+          </ol>
+        </nav>
+      </header>
+      <DrawerMenu btnRef={btnRef} isOpen={isOpen} onClose={onClose}>
+        <ol className={styles['drawer-list']}>
+          <HeaderListItems navLinks={navLinks} />
+        </ol>
+      </DrawerMenu>
+    </>
+  );
+};
 
 export default Header;
