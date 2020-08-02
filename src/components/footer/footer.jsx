1c09f41f516b28b621a26864f53bf2325a3e4abd
Sean Harte <scsharte@gmail.com>
2020-08-02 18:04:46 -0400

    changed up footer layout

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,19 +3,21 @@ import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => (
   <footer className={styles['jsx-footer']}>
-    <div className={styles.copyright}>
-      <small>&copy; 2020, Example Corporation</small>
-    </div>
-    <div className={styles.social}>
-      <a aria-label="facebook" href="#">
-        <FaFacebookF />
-      </a>
-      <a aria-label="Instagram" href="#">
-        <FaInstagram />
-      </a>
-      <a aria-label="Youtube" href="#">
-        <FaYoutube />
-      </a>
+    <div className={styles['inner-container']}>
+      <div className={styles.copyright}>
+        <small>&copy; 2020, Example Corporation</small>
+      </div>
+      <div className={styles.social}>
+        <a aria-label="facebook" href="#">
+          <FaFacebookF />
+        </a>
+        <a aria-label="Instagram" href="#">
+          <FaInstagram />
+        </a>
+        <a aria-label="Youtube" href="#">
+          <FaYoutube />
+        </a>
+      </div>
     </div>
   </footer>
 );
