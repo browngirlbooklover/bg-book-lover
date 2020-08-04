b9036ae58ce5a8027a90830a885707c8d8b382dd
Sean Harte <scsharte@gmail.com>
2020-08-04 01:32:40 -0400

    added video to page, added modal

--- src/pages/reviews/index.jsx
+++ src/pages/reviews/index.jsx
@@ -1,18 +1,32 @@
 import { getPageProps } from '../../lib/api';
 import { motion } from 'framer-motion';
 import { pageVariants } from '../../lib/motionVariants';
+import VideoPlayer from '../../components/videoPlayer/videoPlayer';
+import styles from './reviews.module.scss';
 
 const Reviews = ({ data }) => (
   <motion.div
+    className={styles.layout}
     initial="initial"
     animate="enter"
     exit="exit"
     variants={pageVariants}
   >
-    Reviews
+    <h1 className="header-1">{data?.header}</h1>
+    <div className={styles['video-container']}>
+      {console.log(data)}
+      {data?.videos.map((v) => (
+        <VideoPlayer
+          image={v.poster}
+          title={v.title}
+          date={v.date}
+          link={v.videoLink}
+        />
+      ))}
+    </div>
   </motion.div>
 );
 
-export const getStaticProps = async () => getPageProps('index');
+export const getStaticProps = async () => getPageProps('reviews');
 
 export default Reviews;
