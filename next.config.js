398929053fc588a010f689ab8849aefa750ab403
Sean Harte <scsharte@gmail.com>
2020-08-09 19:33:51 -0400

    added env in build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
   sassOptions: {
     postcssLoaderOptions: {},
+  },
+  env: {
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    REPO_FULL_NAME: process.env.REPO_FULL_NAME,
+    BASE_BRANCH: process.env.BASE_BRANCH,
   },
   target: 'serverless',
   webpack: (config, { isServer }) => {
